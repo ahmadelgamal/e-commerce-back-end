46ae9fcef93e20a9185dc6c15900fca400981d14
Ahmad El Gamal <ahmadelgamal@gmail.com>
2021-01-18 21:36:59 -0800

    fills up part of category-routes. I seeded and tested the /api/categories/ route and it's working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,13 +6,38 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  Category.findAll();
-
+  Category
+    .findAll({
+      include: [Product]
+    })
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch((err) => {
+      console.log(error);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category
+    .findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [Product]
+    })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: "No category found with this id" });
+        return;
+      }
+      res.json(dbCategoryData)
+    })
+    .catch((err) => {
+      console.log(error);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  })
