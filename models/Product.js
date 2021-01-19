90b468aeed332db5765649490bb134d228533fa0
Ahmad El Gamal <ahmadelgamal@gmail.com>
2021-01-19 02:49:33 -0800

    Fixes decimals not showing in product prices

--- models/Product.js
+++ models/Product.js
@@ -20,7 +20,7 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10,2),
       allowNull: false,
       validate: {
         isDecimal: true
